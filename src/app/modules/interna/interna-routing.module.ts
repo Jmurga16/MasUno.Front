11d35f29f346e179203519a605425063dccd1e8a
Jose Murga <jemurgap@bcds.net>
2024-05-31 06:39:56 -0500

    Interna base

--- src/app/modules/interna/interna-routing.module.ts
+++ src/app/modules/interna/interna-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PhpComponent } from './pages/php/php.component';
+import { PythonComponent } from './pages/python/python.component';
+import { DesignComponent } from './pages/design/design.component';
 
 const routes: Routes = [
-
+  {
+    path: 'php',
+    component: PhpComponent,
+  },
+  {
+    path: 'python',
+    component: PythonComponent,
+  },
+  {
+    path: 'design',
+    component: DesignComponent,
+  }
 ];
 
 @NgModule({
