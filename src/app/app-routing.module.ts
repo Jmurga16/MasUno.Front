5f7c3ca6240b3e9292e57948efa97e86c6e3cadc
Jose Murga <jemurgap@bcds.net>
2024-05-31 03:46:57 -0500

    Header y Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'principal',
+    loadChildren: () =>
+      import('./modules/principal/principal.module').then((m) => m.PrincipalModule),
+  },
+  {
+    path: 'interna',
+    loadChildren: () =>
+      import('./modules/interna/interna.module').then((m) => m.InternaModule),
+  },
+  {
+    path: '',
+    redirectTo: 'principal',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'principal',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
